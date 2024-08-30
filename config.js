99ba5f7c0d2410721f8c235e9dda954eea90e044
Maroof66 <76508085+Maroof66@users.noreply.github.com>
2024-08-30 18:35:35 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    serverID: "1043162524071243839", // Server ID in which you'll play tea
+    serverID: "", // Server ID in which you'll play tea
     MudaeID: "432610292342587392" //Mudae's User ID
 };
